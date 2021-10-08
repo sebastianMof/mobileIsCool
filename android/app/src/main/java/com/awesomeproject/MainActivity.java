3c96a41f842608b8a77ea045d4fdeaf18d39b0ce
sebastianMof <sebastian.mofre@gmail.com>
2021-10-08 12:16:21 -0300

    remove bundle null

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -4,14 +4,6 @@ import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(null);
-  }
   @Override
   protected String getMainComponentName() {
     return "AwesomeProject";
